b7eadaa607241af8923bd7add08433d2f10d4143
Steve Clark <steve@bigsteve.us>
2017-02-15 14:12:14 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# aix_hardening
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: aix_hardening
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
