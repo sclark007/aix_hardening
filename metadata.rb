3c07dd56bd02d807c911aafc41c44189b6add19a
Steve Clark <steve@bigsteve.us>
2017-02-21 13:44:40 -0500

    bump v

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'steve@bigsteve.us'
 license 'all_rights'
 description 'Installs/Configures aix_hardening'
 long_description 'Installs/Configures aix_hardening'
-version '0.1.0'
+version '0.1.1'
 
 issues_url 'https://github.com/sclark007/aix_hardening/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/sclark007/aix_hardening' if respond_to?(:source_url)
